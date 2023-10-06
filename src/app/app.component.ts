0e5e9ba207cd5a45603c6f7465961a4f4ac9160b
Angel <angel.aven.c@gmail.com>
2023-10-06 16:33:03 -0600

    Detención de cambios en formularios
    Detención de cambios en formularios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import {Product} from "./models/product.model";
 })
 export class AppComponent {
   imgParent = 'https://images.unsplash.com/photo-1696321832425-a6871f33420f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3987&q=80/'
-
+  showImage = true
   products: Product[] = [
     {
       id: '1',
@@ -39,5 +39,9 @@ export class AppComponent {
 
   loadedImg(img: String){
     console.log('El padre ya sabe que cargo la imagen => ', img)
+  }
+
+  toggleImage(){
+    this.showImage = !this.showImage
   }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,14 @@
-import {Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  AfterViewInit,
+  OnDestroy,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,9 +17,22 @@ import {Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit
 })
 export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
-  @Input() image: String = ''
-  imageDefault = 'https://images.unsplash.com/photo-1696247835082-2d6f7e528bfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=4140&q=80'
+  image: string = ''
+
+  @Input('image')
+  set changeImage(newImage: string){
+    // Esto se ejecta cuando cambie el input image
+    this.image = newImage
+    console.log('- changeImage -')
+
+    // code
+  }
+
+  @Input() alt: String = ''
   @Output() loaded = new EventEmitter<String>()
+  imageDefault = 'https://images.unsplash.com/photo-1696247835082-2d6f7e528bfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=4140&q=80'
+  counter = 0
+  counterFunction: number | undefined
 
   constructor() {
     // Aqui no van cosas asincronas
@@ -23,14 +46,17 @@ export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy
     // Podemos correr cosas asincronas
     // Corre una sola vez antes de que inicie el render
     console.log('- ngOnInit -')
-    console.log('imgValue => ', this.image)
+    this.counterFunction = window.setInterval(() => {
+      this.counter ++
+      console.log('Se corre counter')
+    }, 1000)
   }
 
-  ngOnChanges(){
+  ngOnChanges(changes: SimpleChanges){
     // Corre antes del render del componente y durante
     // Actualiza los cambios en los inputs
     console.log('- ngOnChanges -')
-    console.log('imgValue => ', this.image)
+    console.log('changes => ', changes)
   }
 
   ngAfterViewInit(){
@@ -44,6 +70,7 @@ export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy
     //Cuando eliminamos el componente
     // Cuando el componente deja de existir
     console.log('- ngOnDestroy -')
+    window.clearInterval(this.counterFunction)
   }
 
   imgError(){
