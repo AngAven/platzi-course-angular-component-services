f1a7518cdcc7347768b903007e25edb4caac52d2
Angel <angel.aven.c@gmail.com>
2023-10-06 13:55:38 -0600

    Enviar información del hijo al padre y transmisión de eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  imgParent = 'https://images.unsplash.com/photo-1696321832425-a6871f33420f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3987&q=80/';
+  imgParent = 'https://images.unsplash.com/photo-1696321832425-a6871f33420f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3987&q=80/'
+
+  loadedImg(img: String){
+    console.log('El padre ya sabe que cargo la imagen => ', img)
+  }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,11 +7,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
 
-  @Input() image: String = 'valor inicial'
+  @Input() image: String = ''
+  imageDefault = 'https://images.unsplash.com/photo-1696247835082-2d6f7e528bfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=4140&q=80'
+  @Output() loaded = new EventEmitter<String>()
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  imgError(){
+    this.image = this.imageDefault
+  }
+
+  imgLoaded(){
+    console.log('Imagen cargada y se emitirá el evento al padre')
+    this.loaded.emit(this.image)
+  }
 }
