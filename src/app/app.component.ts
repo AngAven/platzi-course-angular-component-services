1ba6ff5aaa3e855d5803dfd28ff838e562844228
Angel <angel.aven.c@gmail.com>
2023-10-06 13:21:51 -0600

    Uso de "ng g c new-component" y comunicación de información del padre al hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'platzi-course-angular-component-services';
+  imgParent = 'https://images.unsplash.com/photo-1696321832425-a6871f33420f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3987&q=80/';
 }
